15df7c93c1c18bd74dabda2d4af4ad692fc59107
KingJamesGOAT <sbstevebenjamin@gmail.com>
2026-05-22 11:41:09 +0200

    Add minimal Vue configuration

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue'; // Importation du plugin Vue
 
 export default defineConfig({
     plugins: [
+        vue(), // Initialisation du plugin Vue
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
